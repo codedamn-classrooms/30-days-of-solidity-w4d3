db5fb4afa830369d7effa10efe6857788861eb8d
srivastavakshitijprofessional@gmail.com <srivastavakshitijprofessional@gmail.com>
2022-04-25 18:41:17 +0530

    initial

---
+++ contracts/Day3.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract Day3 {
+    function hcf(uint256 n1, uint256 n2) public pure returns (uint256) {
+        if (n2 != 0) {
+            return hcf(n2, n1 % n2);
+        } else {
+            return n1;
+        }
+    }
+}
