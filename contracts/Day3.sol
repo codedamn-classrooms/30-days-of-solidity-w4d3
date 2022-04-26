dd16a73746e9816b54a68c2c1e85fe4b13ca7ff2
kshitijofficial <52281964+kshitijofficial@users.noreply.github.com>
2022-04-26 11:14:26 +0530

    Update Day3.sol

--- contracts/Day3.sol
+++ contracts/Day3.sol
@@ -2,11 +2,5 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Day3 {
-    function hcf(uint256 n1, uint256 n2) public pure returns (uint256) {
-        if (n2 != 0) {
-            return hcf(n2, n1 % n2);
-        } else {
-            return n1;
-        }
-    }
+    
 }
